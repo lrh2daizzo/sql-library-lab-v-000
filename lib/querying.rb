7d79da3bee2d6fd717941bd9c48a46deee7824bd
Luis Herrera <herrera.lrh@gmail.com>
2019-08-15 02:56:50 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,3 +16,11 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM authors JOIN series ON
   series.author_id = authors.id JOIN subgenres ON series.subgenre_id = subgenres.id;"
 end
+
+def select_series_title_with_most_human_characters
+  "SELECT title FROM series JOIN books ON books.series_id = series.id
+  JOIN character_books ON character_books.book_id = books.id
+  JOIN characters ON character_books.character_id = characters.id
+  WHERE character.species = 'human' GROUP BY series.title
+  ORDER BY COUNT(*) DESC LIMIT 1;"
+end
