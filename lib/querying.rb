6c5a615cbc7b49cf9e25f4b24d3ba20dd0c39df6
Luis Herrera <herrera.lrh@gmail.com>
2019-08-15 03:01:37 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,3 +24,9 @@ def select_series_title_with_most_human_characters
   WHERE characters.species = 'human' GROUP BY series.title
   ORDER BY COUNT(*) DESC LIMIT 1;"
 end
+
+def select_character_names_and_number_of_books_they_are_in
+  "SELECT characters.name, COUNT(*) AS book_count FROM character_books JOIN
+  characters ON character.character_id = characters.id GROUP BY characters.name
+  ORDER BY book_count DESC;"
+end
